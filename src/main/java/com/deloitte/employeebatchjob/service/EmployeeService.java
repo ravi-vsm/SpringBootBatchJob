14be9f7be80b9b54dca20c70f048f44e2932407e
ravi-vsm <ravi.mca44@gmail.com>
2020-05-19 22:42:53 +0530

    initial checkin

---
+++ src/main/java/com/deloitte/employeebatchjob/service/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.deloitte.employeebatchjob.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author RAVI YARAMATI
+ *
+ */
+@Service
+public class EmployeeService {
+
+}
